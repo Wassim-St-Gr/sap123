e256767af3de1732e229575186f372beb27f3c63
unknown <w.brahim@striped-giraffe.com>
2018-10-04 11:39:45 +0200

    Extend the ServiceLayer

--- bin/platform/bootstrap/gensrc/concerttours/model/BandModel.java
+++ bin/platform/bootstrap/gensrc/concerttours/model/BandModel.java
@@ -1,7 +1,7 @@
 /*
  * ----------------------------------------------------------------
  * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
- * --- Generated at Oct 2, 2018 2:03:27 PM                      ---
+ * --- Generated at Oct 4, 2018 11:37:59 AM                     ---
  * ----------------------------------------------------------------
  */
 package concerttours.model;
